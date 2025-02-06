d727c69860492ffbc486bfbc72023e0aa2e44201
Vikasmummidi <61915117+Vikasmummidi@users.noreply.github.com>
2025-02-06 17:49:36 +0530

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -4,7 +4,7 @@ $connection = mysqli_connect("mysql-db", "root", "your_root_password", "practice
     if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+//echo "Connected successfully";
 
     // Selecting a database 
 
